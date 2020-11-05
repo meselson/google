4e11eb1648797beba18c1284e7fa52e7a357fa08
meselson <meselson@126.com>
2020-11-06 01:26:35 +0800

    Update getfree.py
    fix bug

--- getfree.py
+++ getfree.py
@@ -2,8 +2,7 @@ import time
 import requests
 today = time.strftime("%m%d")
 
-#url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}clash.yml'
-url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/1103clash.yml'
+url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}clash.yml'
 info = requests.get(url).text
 with open('free.yml', 'w', encoding = 'UTF-8') as f:
     f.write(info)
