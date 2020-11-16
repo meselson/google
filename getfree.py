f8145c047975f05b04e9f811452bf06ccf7e2d3c
meselson <meselson@126.com>
2020-11-16 09:59:47 +0800

    Update getfree.py

--- getfree.py
+++ getfree.py
@@ -3,6 +3,9 @@ import requests
 today = time.strftime("%m%d")
 
 url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}clash.yml'
-info = requests.get(url).text
-with open('free.yml', 'w', encoding = 'UTF-8') as f:
-    f.write(info)
+info = requests.get(url)
+if info.status_code == '200':
+    with open('free.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info)
+else:
+    print (f'{today} no data')
