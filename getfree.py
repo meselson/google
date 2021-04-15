d055a0b807ed970a54db9501ad7756621afdf10c
meselson <meselson@126.com>
2021-04-15 11:20:56 +0800

    Update getfree.py

--- getfree.py
+++ getfree.py
@@ -6,6 +6,6 @@ url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}c
 info = requests.get(url)
 if info.status_code == 200:
     with open('free.yml', 'w', encoding = 'UTF-8') as f:
-        f.write(info.text)
+        f.write(info.text.replace("(欢迎订阅Youtube破解资源君)",""))
 else:
     print (f'{today} no data')
